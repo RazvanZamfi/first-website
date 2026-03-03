0d0c9122ccc96e7ee9d4c9df99819d7ed15f433c
RazvanInterrobang <razvan@interrobang.ro>
2026-03-03 16:59:34 +0000

    Initial commit

---
+++ code.js
@@ -0,0 +1,13 @@
+document.getElementById("btn").addEventListener("click", function () {
+  document.getElementById("text").style.color = "blue";
+});
+const name="World";
+console.log("Hello, " + name + "!");
+
+const a = 10;
+const b = 3;
+console.log("The sum of a and b is: " + (a+b));
+
+const fruits = ["apple", "banana", "cherry"];
+console.log(fruits);
+console.log(fruits.length);
